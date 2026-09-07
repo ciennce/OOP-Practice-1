e2d1e3d654b87ec19e953c1f58f637dbac3f7b7e
ciennce <maxileonhoppe@googlemail.com>
2026-09-07 09:42:07 +0200

    Added class "Account" with attributes.

---
+++ OOP-Practice-1/Account.cs
@@ -0,0 +1,29 @@
+﻿namespace OOP_Practice_1
+{
+    internal class Account
+    {
+        public string Name { get; set; }
+        public int Iban { get; set; }
+        protected int Balance { get; set; }
+
+        public Account(string Name, int Iban, int Balance)
+        {
+            this.Name = Name;
+            this.Iban = Iban;
+            this.Balance = Balance;
+        }
+
+        public void Payout(string Name, int Iban, int PayoutBalance)
+        {
+            if(Name == this.Name || Iban == this.Iban)
+            {
+                Balance += PayoutBalance;
+            }
+            else
+            {
+                Console.WriteLine("This Account or Iban does not exit. Please try again.");
+            }
+        }
+
+    }
+}
