5ed146f36b438b2b6106571e43633e246e6af474
Jan.Hoppe <hopp1395@gmail.com>
2026-09-07 21:42:31 +0200

    änderunbgen

--- OOP-Practice-1/Bank/Account.Booking.cs
+++ OOP-Practice-1/Bank/Account.Booking.cs
@@ -1,34 +1,25 @@
 ﻿using OOP_Practice_1.Gui;
 namespace OOP_Practice_1.Bank
 {
-    class Account
+    public partial class Account
     {
-        public string Name { get; }
-
-        public string Iban { get; }
-
-        public Account(string name, string iban, int balance)
-        {
-            this.Name = name;
-            this.Iban = iban;
-            this.Balance = balance;
-        }
-
         public void Payout()
         {
             Console.WriteLine("How much would you like to payout?");
-            int payoutBalance = Convert.ToInt32(Console.ReadLine());
+            var payoutBalance = Convert.ToInt32(Console.ReadLine());
 
             if (payoutBalance > Balance)
             {
                 Console.Clear();
                 Console.WriteLine("Insufficient funds.");
-            }else
+            }
+            else
             {
                 Console.Clear();
                 Balance -= payoutBalance;
                 Console.WriteLine($"Your new balance is {Balance}$");
             }
+
             GUI.AwaitEnter();
 
         }
@@ -36,13 +27,20 @@ namespace OOP_Practice_1.Bank
         public void Deposit()
         {
             Console.WriteLine("How much would you like to deposit?:");
-            int depositBalance = Convert.ToInt32(Console.ReadLine());
-            
+
+            if (!decimal.TryParse(Console.ReadLine(), out var depositBalance))
+            {
+                Console.WriteLine("Ungültige Eingabe. Es sind nur Zahlen erlaubt!");
+                GUI.AwaitEnter();
+                return;
+            }
+
             if (depositBalance < 0)
             {
                 Console.Clear();
                 Console.WriteLine("Invalid amount.");
-            } else
+            }
+            else
             {
                 Console.Clear();
                 Balance += depositBalance;
